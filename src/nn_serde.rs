00df982ba48945d1a7aa101fc38d2fd09c528003
AngryBeltMan <groupydrone@gmail.com>
2023-08-08 19:38:35 -0400

    Removed excess debug info

--- src/nn_serde.rs
+++ src/nn_serde.rs
@@ -32,7 +32,6 @@ impl NNSerde for NeuralNetwork {
         let mut file = File::open(path)?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
-        eprintln!("{:#?}", contents);
         let nn: Self = from_str(&contents)?;
         Ok(nn)
     }
